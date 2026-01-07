0a200400d91408959b1a0fe4f794b01d9ceacdc1
babulpro <sh8657706@gmail.com>
2026-01-07 12:31:16 +0600

    order cancel  api create

--- src/lib/authFunction/JwtHelper.js
+++ src/lib/authFunction/JwtHelper.js
@@ -1,7 +1,7 @@
 import { jwtVerify, SignJWT } from "jose"
 
  export async function CreateJwtToken(email,id,days) {
-    const Secret= new TextEncoder().encode(process.env.jwtSecret)
+    const Secret= new TextEncoder().encode("123-xyz")
     let token = await new SignJWT({email,id})
                 .setProtectedHeader({alg:'HS256'})
                 .setIssuedAt()
@@ -15,7 +15,7 @@ import { jwtVerify, SignJWT } from "jose"
 
 
 export async function DecodedJwtToken(token) {
-    const Secret= new TextEncoder().encode(process.env.jwtSecret)
+    const Secret= new TextEncoder().encode("123-xyz")
     const decodedToken = await jwtVerify(token,Secret)
     return decodedToken['payload']   
     
